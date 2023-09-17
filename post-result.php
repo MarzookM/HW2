6ae68ce6f06cb3fbd5b22d47a35ab0f635103063
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-09-17 17:58:19 -0500

    Create post-result.php

---
+++ post-result.php
@@ -0,0 +1,19 @@
+<?php
+$pageTitle = "Post-result";
+include "view-header.php";
+?> 
+<h1>Post result</h1>  
+<?php
+if(isset($_POST['my-name'])){
+  ?>
+  <p> The value send is:</p>
+<?php
+  echo $_Post['my-name'};
+}
+else {
+  ?>
+<p>Nothing posted to the page</p>
+<?php
+}
+include "view-footer.php";
+?> 
